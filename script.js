693204307ccdd45831f1a867256d22667e523c88
Stiliyan97 <stiliyan199713@gmail.com>
2025-02-03 08:01:11 +0200

    Add style to the add book panel

---
+++ script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function Book (title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages
+}
+
+const bookTitleInput = document.getElementById("book-title");
+
+const bookTitle = bookTitleInput.input;
+
+console.log(bookTitle);
+function addBookToLibrary() {
+
+}
